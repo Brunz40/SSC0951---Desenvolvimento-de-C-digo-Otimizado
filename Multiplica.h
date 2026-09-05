bd40c7c2b3a4402a559a391ad6457bb233e4be89
bruno <brunodcozac@usp.br>
2026-09-05 14:48:40 -0300

    definiçãod as funções pedidas no exercicio

---
+++ Multiplica.h
@@ -0,0 +1,8 @@
+float multiplica_simples_estático(float A, float B, int n);
+float** multiplica_simples_dinâmico(float** A, float** B, int n);
+float multiplica_unroll_estático(float A, float B, int n);
+float** multiplica_unroll_dinâmico(float** A, float** B, int n);
+float multiplica_interchange_estático(float A, float B, int n);
+float **multiplica_interchange_dinâmico(float **A, float **B, int n);
+float multiplica_tiling_estático(float A, float B, int n);
+float **multiplica_tiling_dinâmico(float **A, float **B, int n);
